b63b37bf6499e186b47de7b121237c00195186d2
Karthik Nishanth <nishanthkarthik@live.com>
2016-06-11 17:59:21 +0530

    added infinite auth

--- py-auth/auth.py
+++ py-auth/auth.py
@@ -2,7 +2,9 @@ import re
 import requests
 import http.client
 import getpass
-
+import atexit
+import threading
+import time
 import logging as log
 import coloredlogs
 coloredlogs.install()
@@ -22,7 +24,6 @@ class NetAccess(object):
 
     def __init__(self):
         super(NetAccess, self).__init__()
-        log.info('Setting logged In status as False')
         self.loggedIn = False
 
     def input_credentials(self):
@@ -37,15 +38,18 @@ class NetAccess(object):
             if r_refresh.status_code == 200:
                 log.info('Refreshing with %s', self.refresh_url)
             else:
-                log.error('Refresh failed with %s at %s', r_refresh.status_code, self.refresh_url)
+                log.error('Refresh failed with %s at %s',
+                          r_refresh.status_code, self.refresh_url)
 
     def logout(self):
         if self.loggedIn:
             r_logout = requests.get(self.logout_url, verify=False)
             if r_logout.status_code == 200:
                 log.warning('Logged out with %s', self.logout_url)
+                self.loggedIn = False
             else:
-                log.error('Logout failed with %s at %s', r_logout.status_code, self.logout_url)
+                log.error('Logout failed with %s at %s',
+                          r_logout.status_code, self.logout_url)
 
     def setup(self):
         try:
@@ -79,8 +83,10 @@ class NetAccess(object):
                 self.refresh_url = p_refresh.search(r_auth.text).group('url')
                 self.logout_url = p_logout.search(r_auth.text).group('url')
                 self.loggedIn = True
-                log.info('KeepAlive from %s', p_refresh.search(r_auth.text).group('url'))
-                log.info('Logout from %s', p_logout.search(r_auth.text).group('url'))
+                log.info('KeepAlive from %s', p_refresh.search(
+                    r_auth.text).group('url'))
+                log.info('Logout from %s', p_logout.search(
+                    r_auth.text).group('url'))
                 return True
             elif r_bing.status_code == 200:
                 log.warning('Internet works already!')
@@ -89,11 +95,23 @@ class NetAccess(object):
             return False
 
 
+def periodic_refresh(_netaccess):
+    _netaccess.refresh()
+
+
+def clean_exit(_netaccess):
+    _netaccess.logout()
+    #_timerthread.cancel()
+
+
 def main():
     netaccess = NetAccess()
     if netaccess.input_credentials():
-        netaccess.refresh()
-        netaccess.logout()
+        #timer = threading.Timer(5.0, periodic_refresh, [netaccess])
+        atexit.register(clean_exit, netaccess)
+        while True:
+            periodic_refresh(netaccess)
+            time.sleep(5)
     return
 
 if __name__ == '__main__':
